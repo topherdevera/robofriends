7a24497ae1574d7abfefc3db350b0085a592ab2d
Christopher de Vera <topher@fcfminerals.local>
2020-02-20 09:04:02 +0800

    added new project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Topher de Vera',
+		username: 'tophs',
+		email: 'topher@fcfminerals.com'
+	},
+	{
+		id: 2,
+		name: 'Cassey Johanna de Vera',
+		username: 'cheng',
+		email: 'cheng@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Caisser Johann de Vera',
+		username: 'caicai',
+		email: 'cj@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Joan Taba',
+		username: 'negs',
+		email: 'negs@gmail.com'
+	},
+	{
+	    id: 5,
+	    name: 'Chelsey Dietrich',
+	    username: 'Kamren',
+	    email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+	    id: 6,
+	    name: 'Mrs. Dennis Schulist',
+	    username: 'Leopoldo_Corkery',
+	    email: 'Karley_Dach@jasper.info'
+    },
+    {
+	    id: 7,
+	    name: 'Kurtis Weissnat',
+	    username: 'Elwyn.Skiles',
+	    email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+	    id: 8,
+	    name: 'Nicholas Runolfsdottir V',
+	    username: 'Maxime_Nienow',
+	    email: 'Sherwood@rosamond.me'
+    },
+    {
+	    id: 9,
+	    name: 'Glenna Reichert',
+	    username: 'Delphine',
+	    email: 'Chaim_McDermott@dana.io'
+    },
+    {
+	    id: 10,
+	    name: 'Clementina DuBuque',
+	    username: 'Moriah.Stanton',
+	    email: 'Rey.Padberg@karina.biz'
+    }
+]
